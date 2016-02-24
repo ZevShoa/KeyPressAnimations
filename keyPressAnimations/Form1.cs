9d567b63c284ea2dfeb0e0e307c8ba54f4d3f9cf
Zevshoa <zshoag@gmail.com>
2016-02-24 09:41:57 -0500

    Finished

--- keyPressAnimations/Form1.cs
+++ keyPressAnimations/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-/* Created by Mr. T
+/* Created by ZEV
  * to demonstrate how to use key presses to control the animation
  * of an object on screen
  */
@@ -15,13 +15,16 @@ using System.Windows.Forms;
 namespace keyPressAnimations
 {
     public partial class Form1 : Form
+
     {
+        Image[] guy = new Image[4];
+        int guyNum;
         //initial starting points for black rectangle
         int drawX = 100;
         int drawY = 200;
 
         //determines whether a key is being pressed or not
-        Boolean leftArrowDown, downArrowDown, rightArrowDown, upArrowDown;
+        Boolean aArrowDown, sArrowDown, dArrowDown, wArrowDown;
 
         //create graphic objects
         SolidBrush drawBrush = new SolidBrush(Color.Black);
@@ -29,7 +32,10 @@ namespace keyPressAnimations
         public Form1()
         {
             InitializeComponent();
-
+            guy[0] = Properties.Resources.RedGuyDown;
+            guy[1] = Properties.Resources.RedGuyRight;
+            guy[2] = Properties.Resources.RedGuyUp;
+            guy[3] = Properties.Resources.RedGuyLeft;
             //start the timer when the program starts
             gameTimer.Enabled = true;
             gameTimer.Start();
@@ -40,17 +46,17 @@ namespace keyPressAnimations
             //check to see if a key is pressed and set is KeyDown value to true if it has
             switch (e.KeyCode)
             {
-                case Keys.Left:
-                    leftArrowDown = true;
+                case Keys.A:
+                    aArrowDown = true;
                     break;
-                case Keys.Down:
-                    downArrowDown = true;
+                case Keys.S:
+                    sArrowDown = true;
                     break;
-                case Keys.Right:
-                    rightArrowDown = true;
+                case Keys.D:
+                    dArrowDown = true;
                     break;
-                case Keys.Up:
-                    upArrowDown = true;
+                case Keys.W:
+                    wArrowDown = true;
                     break;
                 default:
                     break;
@@ -63,17 +69,17 @@ namespace keyPressAnimations
             //check to see if a key has been released and set its KeyDown value to false if it has
             switch (e.KeyCode)
             {
-                case Keys.Left:
-                    leftArrowDown = false;
+                case Keys.A:
+                    aArrowDown = false;
                     break;
-                case Keys.Down:
-                    downArrowDown = false;
+                case Keys.S:
+                    sArrowDown = false;
                     break;
-                case Keys.Right:
-                    rightArrowDown = false;
+                case Keys.D:
+                    dArrowDown = false;
                     break;
-                case Keys.Up:
-                    upArrowDown = false;
+                case Keys.W:
+                    wArrowDown = false;
                     break;
                 default:
                     break;
@@ -84,21 +90,25 @@ namespace keyPressAnimations
         {
             //checks to see if any keys have been pressed and adjusts the X or Y value
             //for the rectangle appropriately
-            if (leftArrowDown == true)
+            if (aArrowDown == true)
             {
                 drawX--;
+                guyNum = 3;
             }
-            if (downArrowDown == true)
+            if (sArrowDown == true)
             {
                 drawY++;
+                guyNum = 0;
             }
-            if (rightArrowDown == true)
+            if (dArrowDown == true)
             {
                 drawX++;
+                guyNum = 1;
             }
-            if (upArrowDown == true)
+            if (wArrowDown == true)
             {
                 drawY--;
+                guyNum = 2;
             }
 
             //refresh the screen, which causes the Form1_Paint method to run
@@ -110,7 +120,9 @@ namespace keyPressAnimations
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             //draw rectangle to screen
-            e.Graphics.FillRectangle(drawBrush, drawX, drawY, 10, 20);
+            
+            e.Graphics.DrawImage(guy[guyNum], drawX, drawY);
+
         }
 
     }
